3197f48e8d5d45bdfb4dc8253cb0a19dc557fa51
MoonMan42 <miller42matt@gmail.com>
2019-06-01 12:19:24 -0400

    Set clock to 24 hour period for testing.

--- Clock/MainWindow.xaml.cs
+++ Clock/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Clock
@@ -28,7 +17,7 @@ namespace Clock
             DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1),
                 DispatcherPriority.Normal, delegate
                 {
-                    clockLabel.Content = DateTime.Now.ToString("hh:mm:ss tt");
+                    clockLabel.Content = DateTime.Now.ToString("HH:mm:ss"); // add tt for am and pm 
                 }, this.Dispatcher);
 
         }
